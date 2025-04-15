a641c8a05b9827ea49dc1ad22faaee0a88df776c
Mikael Björkman <mikael@bjorkman.xyz>
2025-04-15 07:48:44 +0200

    add debounce

--- pico/+irq/src/gpio_irq.cpp
+++ pico/+irq/src/gpio_irq.cpp
@@ -1,14 +1,24 @@
 #include "pico/stdlib.h"
-#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include <iostream>
 
 // Define the pins for the button and LED
 #define BUTTON_PIN 17
 #define LED_PIN 16
+#define DEBOUNCE_TIME_MS 200
+
+volatile uint32_t last_interrupt_time = 0;
 
 // Interrupt Service Routine for button press
 void gpio_callback(uint gpio, uint32_t events) {
-    bool led_on = gpio_get(LED_PIN);
-    gpio_put(LED_PIN, !led_on);
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+
+    if (current_time - last_interrupt_time > DEBOUNCE_TIME_MS) {
+	last_interrupt_time = current_time;
+	std::cout << "Button pressed at " << current_time << std::endl;
+	bool led_on = gpio_get(LED_PIN);
+	gpio_put(LED_PIN, !led_on);
+    }
 }
 
 int main() {
@@ -27,10 +37,10 @@ int main() {
 
     // Set the interrupt handler for the button
     // The callback handles all GPIO interrupts, so check the pin in the function.
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true, gpio_callback);
 
     while (true) {
-        tight_loop_contents();
+	sleep_ms(100);
     }
 
     return 0;
